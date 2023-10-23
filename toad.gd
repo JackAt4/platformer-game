5536471c3dcf91d7e844db89f85b86b3a2058fae
Jack@ <148102997+JackAt4@users.noreply.github.com>
2023-10-23 11:14:54 -0500

    Player Death
    Added player death and other stuff

--- toad.gd
+++ toad.gd
@@ -3,8 +3,11 @@ extends CharacterBody2D
 const SPEED = 25.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var is_alive = true
 @onready var animated_sprite_2d = $AnimatedSprite2D
+
+func _ready():
+	add_to_group("Enemy")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -23,4 +26,6 @@ func update_animation():
 
 func _on_hitbox_body_entered(body):
 	if body.is_in_group("Player"):
+		body.velocity.y = -200
+		is_alive = false
 		queue_free()
