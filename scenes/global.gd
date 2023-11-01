16690f65bbb7de34ba6264dda584e9b68b077257
Jack@ <148102997+JackAt4@users.noreply.github.com>
2023-11-01 10:47:01 -0500

    Added Sandal Powerup
    For the love of god why did we have to do this tutorial this was the absolute worst one by far. He doesn't explain most of the code he just copies it in and tells us too as well. I feel like I understand Godot less after that

--- scenes/global.gd
+++ scenes/global.gd
@@ -2,9 +2,17 @@ extends Node
 
 var total_coins = 0
 var player_lives = 3
+enum PlayerState { SMALL, BIG, SANDAL }
+var current_state = PlayerState.SMALL
 
 func spawn_beer_bottle(pos):
 	var BeerBottleScene = load("res://scenes/beer.tscn")
 	var beer_bottle = BeerBottleScene.instantiate()
 	beer_bottle.global_position = pos
 	get_tree().root.add_child(beer_bottle)
+
+func spawn_sandal_powerup(pos):
+	var SandalPowerUpScene = load("res://scenes/sandalpowerup.tscn")
+	var sandal_powerup = SandalPowerUpScene.instantiate()
+	sandal_powerup.global_position = pos
+	get_tree().root.add_child(sandal_powerup)
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,19 +1,24 @@
 extends CharacterBody2D
 
+var is_firing_sandal = false
+var can_fire_sandal = false
 var is_dying = false
 var is_jumping = false
 var is_big = false
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
+var player_direction = 1
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var death_timer = $DeathTimer
+@onready var sandal_fire_timer = $SandalFireTimer
 
 func _ready():
 	add_to_group("Player")
 	death_timer.connect("timeout", Callable(self, "_on_DeathTimer_timeout"))
+	sandal_fire_timer.connect("timeout", Callable(self, "_on_SandalFireTimer_timeout"))
 	
 func _physics_process(delta):
 	if is_dying:
@@ -24,6 +29,10 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 	else:
 		is_jumping = false
+	
+	if Global.current_state == Global.PlayerState.SANDAL and Input.is_action_just_pressed("fire"):
+		fire_sandal()
+	
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -41,25 +50,38 @@ func _physics_process(delta):
 	move_and_slide()
 
 func update_animation(direction):
-	if is_dying:
+	if is_dying or is_firing_sandal:
 		return
 	
-	if is_jumping:
-		animated_sprite_2d.play("jump")
-	elif direction != 0:
-		animated_sprite_2d.flip_h = (direction < 0)
-		animated_sprite_2d.play("run")
-	else:
-		animated_sprite_2d.play("idle")
+	
+	match Global.current_state:
+		Global.PlayerState.SMALL, Global.PlayerState.BIG:
+			if is_jumping:
+				animated_sprite_2d.play("jump")
+			elif direction != 0:
+				animated_sprite_2d.flip_h = (direction < 0)
+				animated_sprite_2d.play("run")
+			else:
+				animated_sprite_2d.play("idle")
+		Global.PlayerState.SANDAL:
+			if is_jumping:
+				animated_sprite_2d.play("sandal_jump")
+			elif direction != 0:
+				animated_sprite_2d.flip_h = (direction < 0)
+				animated_sprite_2d.play("sandal_run")
+			else:
+				animated_sprite_2d.play("sandal_idle")
 
 
 func _on_hitbox_body_entered(body):
 	if body.is_in_group("Enemy") and body.is_alive:
-		if is_big:
-			become_small()
-		else:
-			die()
-
+		match Global.current_state:
+			Global.PlayerState.SMALL:
+				die()
+			Global.PlayerState.BIG:
+				Global.current_state = Global.PlayerState.SMALL
+			Global.PlayerState.SANDAL:
+				Global.current_state = Global.PlayerState.BIG
 func die():
 	if is_dying:
 		return
@@ -92,9 +114,28 @@ func on_DeathTimer_timeout():
 	get_tree().reload_current_scene()
 
 func become_big():
-	is_big = true
+	Global.current_state = Global.PlayerState.BIG
 	self.scale = Vector2(1.5, 1.5)
 	
 func become_small():
-	is_big = false
+	Global.current_state = Global.PlayerState.SMALL
 	self.scale = Vector2(1,1)
+
+func got_sandal():
+	Global.current_state = Global.PlayerState.SANDAL
+
+#Inside fire_sandal function
+func fire_sandal():
+	is_firing_sandal = true
+	print("firing sandal")
+	var sandal = load("res://scenes/sandal.tscn").instantiate()
+	sandal.global_position = Vector2(self.global_position.x, self.global_position.y - 15)
+
+	sandal.set("velocity", Vector2(500 * player_direction, 0))
+	print("Sandal fired")
+	get_parent().add_child(sandal)
+	$AnimatedSprite2D.play("sandal_fire")
+	sandal_fire_timer.start(1.0)
+
+func _on_SandalFireTimer_timeout():
+	is_firing_sandal = false
